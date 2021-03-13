a4e47aa58eab47217e5d07b8f86598976a068ddb
TJ <thaddeusjiang@gmail.com>
2021-03-13 21:41:28 +0900

    feat(components/sfooter.vue): create

---
+++ components/SFooter.vue
@@ -0,0 +1,128 @@
+<template>
+  <footer
+    class="px-4 py-8 bg-gray-100 space-y-8 md:space-y-0 md:grid md:grid-cols-4"
+  >
+    <div>
+      <h2 class="text-2xl font-bold">Stoic-Web</h2>
+    </div>
+    <div class="flex flex-wrap md:block md:space-x-0 md:space-y-4">
+      <div class="mr-10 last:mr-0">
+        <a href="#" class="text-black hover:underline hover:text-opacity-50"
+          >店舗情報</a
+        >
+      </div>
+      <div class="mr-10 last:mr-0">
+        <a href="#" class="text-black hover:underline hover:text-opacity-50"
+          >XXXX</a
+        >
+      </div>
+      <div class="mr-10 last:mr-0">
+        <a href="#" class="text-black hover:underline hover:text-opacity-50"
+          >XXXX</a
+        >
+      </div>
+      <div class="mr-10 last:mr-0">
+        <a href="#" class="text-black hover:underline hover:text-opacity-50"
+          >XXXX</a
+        >
+      </div>
+      <div class="mr-10 last:mr-0">
+        <a href="#" class="text-black hover:underline hover:text-opacity-50"
+          >XXXX</a
+        >
+      </div>
+      <div class="mr-10 last:mr-0">
+        <a href="#" class="text-black hover:underline hover:text-opacity-50"
+          >XXXX</a
+        >
+      </div>
+      <div class="mr-10 last:mr-0">
+        <a href="#" class="text-black hover:underline hover:text-opacity-50"
+          >XXXX</a
+        >
+      </div>
+    </div>
+    <div class="flex flex-wrap md:block md:space-x-0 md:space-y-4">
+      <div class="mr-10 last:mr-0">
+        <a href="#" class="text-black hover:underline hover:text-opacity-50"
+          >YYYY</a
+        >
+      </div>
+      <div class="mr-10 last:mr-0">
+        <a href="#" class="text-black hover:underline hover:text-opacity-50"
+          >YYYY</a
+        >
+      </div>
+      <div class="mr-10 last:mr-0">
+        <a href="#" class="text-black hover:underline hover:text-opacity-50"
+          >YYYY</a
+        >
+      </div>
+      <div class="mr-10 last:mr-0">
+        <a href="#" class="text-black hover:underline hover:text-opacity-50"
+          >YYYY</a
+        >
+      </div>
+      <div class="mr-10 last:mr-0">
+        <a href="#" class="text-black hover:underline hover:text-opacity-50"
+          >YYYY</a
+        >
+      </div>
+      <div class="mr-10 last:mr-0">
+        <a href="#" class="text-black hover:underline hover:text-opacity-50"
+          >YYYY</a
+        >
+      </div>
+      <div class="mr-10 last:mr-0">
+        <a href="#" class="text-black hover:underline hover:text-opacity-50"
+          >YYYY</a
+        >
+      </div>
+    </div>
+    <div class="flex flex-wrap md:block md:space-x-0 md:space-y-4">
+      <div class="mr-10 last:mr-0">
+        <a href="#" class="text-black hover:underline hover:text-opacity-50"
+          >ZZZZ</a
+        >
+      </div>
+      <div class="mr-10 last:mr-0">
+        <a href="#" class="text-black hover:underline hover:text-opacity-50"
+          >ZZZZ</a
+        >
+      </div>
+      <div class="mr-10 last:mr-0">
+        <a href="#" class="text-black hover:underline hover:text-opacity-50"
+          >ZZZZ</a
+        >
+      </div>
+      <div class="mr-10 last:mr-0">
+        <a href="#" class="text-black hover:underline hover:text-opacity-50"
+          >ZZZZ</a
+        >
+      </div>
+      <div class="mr-10 last:mr-0">
+        <a href="#" class="text-black hover:underline hover:text-opacity-50"
+          >ZZZZ</a
+        >
+      </div>
+      <div class="mr-10 last:mr-0">
+        <a href="#" class="text-black hover:underline hover:text-opacity-50"
+          >ZZZZ</a
+        >
+      </div>
+      <div class="mr-10 last:mr-0">
+        <a href="#" class="text-black hover:underline hover:text-opacity-50"
+          >ZZZZ</a
+        >
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {},
+})
+</script>
